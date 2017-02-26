4a81b3050b6e6d5f6f7aef5e799a8657bd744e11
Kyle Helle <kjhelle4@gmail.com>
2017-02-26 11:44:02 -0800

    Update music.module.ts for progress and footer components

--- src/app/music/music.module.ts
+++ src/app/music/music.module.ts
@@ -11,6 +11,8 @@ import { AutoCompleteModule } from 'primeng/primeng';
 import { MusicSearchComponent } from './music-search/music-search.component';
 import { MusicPlayerComponent } from './music-player/music-player.component';
 import { MusicDetailsComponent } from './music-details/music-details.component';
+import { MusicProgressComponent } from './music-progress/music-progress.component';
+import { MusicFooterComponent } from './music-footer/music-footer.component';
 import { ApiService } from './shared/api.service';
 
 @NgModule ({
@@ -26,14 +28,18 @@ import { ApiService } from './shared/api.service';
     // expose components
     MusicSearchComponent,
     MusicPlayerComponent,
-    MusicDetailsComponent
+    MusicDetailsComponent,
+    MusicProgressComponent,
+    MusicFooterComponent
   ],
 
   declarations: [
     // declare components
     MusicSearchComponent,
     MusicPlayerComponent,
-    MusicDetailsComponent
+    MusicDetailsComponent,
+    MusicProgressComponent,
+    MusicFooterComponent
   ],
 
   providers: [
