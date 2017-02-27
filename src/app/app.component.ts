8ce12a33b3fa1c133d2f364e020b6cb09ccaa954
Kyle Helle <kjhelle4@gmail.com>
2017-02-27 08:04:02 -0800

    Add OnInit to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+// ./src/app/app.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { MusicService } from './music/shared/music.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent implements OnInit {
+
+  tracks: any[] = [];
+
+  constructor(
+    private musicService: MusicService
+  ) {}
+
+  ngOnInit() {
+    this.musicService.getPlaylistTracks().subscribe(tracks => {
+      this.tracks = tracks;
+      //this.handleRandom();  // TODO: this function not yet created
+    });
+
+    // on song end
+    this.musicService.audio.onended = this.handleEnded.bind(this);
+
+    // on play time update
+    this.musicService.audio.ontimeupdate = this.handleTimeUpdate.bind(this);
+  }
+
 }
